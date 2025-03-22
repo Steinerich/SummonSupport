7d2d1bb2c4ff585bf8d67a9170646c0ff2187f70
Coarse-Biologist <168189263+Coarse-Biologist@users.noreply.github.com>
2025-03-22 15:53:20 -0700

    made circle green and centered

---
+++ SummonSupport/Assets/Scenes/Scripts/summon_SO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Summons", menuName = "Summon_SO")]
+public class summon_SO : ScriptableObject
+{
+    [SerializeField] private int Health;
+
+
+}
