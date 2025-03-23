122cb0d8e691e3f142bddd643a122c6378e94745
Coarse-Biologist <168189263+Coarse-Biologist@users.noreply.github.com>
2025-03-23 15:33:23 -0700

    Added a lot of clean file names

---
+++ SummonSupport/Assets/Scripts/DungeonMaster.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DungeonMaster : MonoBehaviour
+{
+
+    public static SceneHandler sceneHandler;
+    public static EnemySpawner enemySpawner;
+    public static QuestHandler questHandler;
+    public static SummonCrafting summonsCrafting;
+    // public static StatsHandler playerStats;
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
