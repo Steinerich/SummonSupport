94d11371a978e447f3c31669aa4c9d412159fe69
Deku <DerDeku@gmail.com>
2025-03-23 02:01:26 +0100

    added big heal
    smol heal (10) on left click
    big heal (20) on right click
    also cleaned up code a bit

--- SummonSupport/Assets/Scripts/Player/AbilityHandler.cs
+++ SummonSupport/Assets/Scripts/Player/AbilityHandler.cs
@@ -17,23 +17,49 @@ public class AbilityHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckInputs();
+    }
+    private void CheckInputs()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
-
-            if (hit.collider != null)
-            {
-                StatHandler targetScript = hit.collider.GetComponent<StatHandler>();
-                if (targetScript != null)
-                {
-                    targetScript.AddHealth(10);
-                }
-            }
+            SmolHeal();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            BigHeal();
         }
     }
+    private RaycastHit2D GetTargetOnClick()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+        return hit;
+        
+    }
+    private void Heal(int value)
+    {
+        RaycastHit2D hit = GetTargetOnClick();
+        if (hit.collider != null)
+        {
+            StatHandler targetScript = hit.collider.GetComponent<StatHandler>();
+            if (targetScript != null)
+            {
+                targetScript.AddHealth(value);
+            }
+        }
+        
+    }
+    private void SmolHeal()
+    {
+        Heal(10);
+    }
+    private void BigHeal()
+    {
+        Heal(20);
+    }
 
-    public void SpawnMinions()
+    private void SpawnMinions()
     {
         if (prefabMinion != null)
         {
