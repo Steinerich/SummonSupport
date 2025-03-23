619360c38fdc66610ae39b25e9d42d0ffc660803
Deku <DerDeku@gmail.com>
2025-03-23 13:46:27 +0100

    added scenes to project

--- SummonSupport/Assets/QuestHandler.cs
+++ SummonSupport/Assets/QuestHandler.cs
@@ -6,17 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class QuestHandler : MonoBehaviour
 {
-    private StatHandler statHandler;
+    private AbilityHandler abilityHandler;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StatHandler statHandler = GetComponent<StatHandler>();
+        abilityHandler = GameObject.FindWithTag("Player").GetComponent<AbilityHandler>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (statHandler.GetHealth() >= 100)
+        if (abilityHandler.minions[0].GetComponent<StatHandler>().GetHealth() >= 100)
         {
             SceneManager.LoadScene("FirstLevel");
         }
