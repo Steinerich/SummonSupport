c5200ecc8a266bb10c4da9f07a183b1395cfd901
Coarse-Biologist <168189263+Coarse-Biologist@users.noreply.github.com>
2025-03-24 14:44:30 -0700

    Ui working for alchemy work bench

--- SummonSupport/Assets/Scripts/AlchemyBenchUI.cs
+++ SummonSupport/Assets/Scripts/AlchemyBenchUI.cs
@@ -9,15 +9,25 @@ public class AlchemyBenchUI : MonoBehaviour
     private VisualElement root;
     private VisualElement interactWindow;
     private Label interactLabel;
+    private VisualElement craftingUI;
+    private VisualElement craftandUpgrade;
+    private Label instructions;
     public UnityEvent playerUsingUI;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         root = ui.rootVisualElement;
         interactWindow = root.Q<VisualElement>("Interact");
+        craftingUI = root.Q<VisualElement>("CraftingUI");
+        craftandUpgrade = craftingUI.Q<VisualElement>("CraftandUpgrade");
+        instructions = craftandUpgrade.Q<Label>("Instructions");
         interactLabel = interactWindow.Q<Label>("InteractLabel");
         interactWindow.style.display = DisplayStyle.None;
+        craftingUI.style.display = DisplayStyle.None;
+
 
     }
     private void OnTriggerEnter2D(Collider2D other)
@@ -38,20 +48,58 @@ public class AlchemyBenchUI : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Tab))
 
         {
-            Debug.Log("Tab key pressed!");
-            PlayerUsingUI();
+            InteractWithWorkBench();
         }
         //else Debug.Log("player is not pressing tab");
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player")) interactWindow.style.display = DisplayStyle.None;
+        if (other.gameObject.CompareTag("Player"))
+        {
+            interactWindow.style.display = DisplayStyle.None;
+            craftingUI.style.display = DisplayStyle.None;
+        }
     }
 
     private void PlayerUsingUI()
     {
         playerUsingUI?.Invoke();
     }
+    private void InteractWithWorkBench()
+    {
+        PlayerUsingUI();
+        craftingUI.style.display = DisplayStyle.Flex;
+        Button craftButton = craftingUI.Q<Button>("CraftButton");
+        Button upgradeButton = craftingUI.Q<Button>("UpgradeButton");
+        Button recycleButton = craftingUI.Q<Button>("RecycleButton");
+        Button quitButton = craftingUI.Q<Button>("QuitButton");
 
+        craftButton.RegisterCallback<ClickEvent>(e => ShowCraftingOptions());
+        upgradeButton.RegisterCallback<ClickEvent>(e => ShowUpgradeOptions());
+        recycleButton.RegisterCallback<ClickEvent>(e => ShowRecycleOptions());
+        quitButton.RegisterCallback<ClickEvent>(e => QuitAlchemyUI());
+
+    }
+    private void QuitAlchemyUI()
+    {
+        craftingUI.style.display = DisplayStyle.None;
+        PlayerUsingUI();
+    }
+
+    private void ShowCraftingOptions()
+    {
+        instructions.text = "Combine Cores, Ether and Body Parts to make mions.";
+    }
+    private void ShowUpgradeOptions()
+    {
+        instructions.text = "Which Minion would you like to upgrade?";
+    }
+    private void ShowRecycleOptions()
+    {
+        instructions.text = "Which Minion would you like to recycle for components?";
+    }
+
+    //Button backButton = new Button { text = "Back" };
+    //backButton.RegisterCallback<ClickEvent>(a => instructions.text = "");
 
 }
