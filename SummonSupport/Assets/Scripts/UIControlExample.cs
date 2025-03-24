80675b27fc3d0992c953af1cbdb6ba209b92a68f
Coarse-Biologist <168189263+Coarse-Biologist@users.noreply.github.com>
2025-03-23 17:34:42 -0700

    Add script for UI control (example)

---
+++ SummonSupport/Assets/Scripts/UIControlExample.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIControlExample : MonoBehaviour
+{
+
+
+    [SerializeField] UIDocument uiDoc;
+    private VisualElement uiRoot;
+    private ProgressBar healthBar;
+    private ProgressBar powerBar;
+
+    void Start()
+    {
+        uiRoot = uiDoc.rootVisualElement;
+
+        healthBar = uiRoot.Q<ProgressBar>("HealthBar");
+        powerBar = uiRoot.Q<ProgressBar>("PowerBar");
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        healthBar.value -= 1; // will practically be at zero in the blink of an eye
+        powerBar.value -= 1; // will practically be at zero in the blink of an eye
+    }
+}
